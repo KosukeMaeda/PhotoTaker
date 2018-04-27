6d19d51f5e3ca4cc51569a8027cab565234592ba
Kosuke Maeda <maeda.dvlp@gmail.com>
2018-04-27 16:25:44 +0900

    高解像度で表示

--- app/src/main/java/jp/ac/titech/itpro/sdl/phototaker/MainActivity.java
+++ app/src/main/java/jp/ac/titech/itpro/sdl/phototaker/MainActivity.java
@@ -3,7 +3,11 @@ package jp.ac.titech.itpro.sdl.phototaker;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,12 +15,16 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private final static int REQ_PHOTO = 1234;
-    private Bitmap photoImg = null;
+    String mCurrentPhotoPath = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,12 +36,19 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivityForResult(intent, REQ_PHOTO);
                 PackageManager packageManager = getPackageManager();
                 List activities = packageManager
                         .queryIntentActivities(intent, PackageManager.MATCH_DEFAULT_ONLY);
                 if (activities.size() > 0) {
-                    startActivityForResult(intent, REQ_PHOTO);
+                    File photoFile = createImageFile();
+
+                    if (photoFile != null) {
+                        Uri photoURI = FileProvider.getUriForFile(getApplicationContext(),
+                                "jp.ac.titech.itpro.sdl.phototaker.fileprovider",
+                                photoFile);
+                        intent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                        startActivityForResult(intent, REQ_PHOTO);
+                    }
                 } else {
                     Toast.makeText(MainActivity.this,
                             R.string.toast_no_activities, Toast.LENGTH_LONG).show();
@@ -43,9 +58,29 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void showPhoto() {
-        if (photoImg == null) return;
+        if (mCurrentPhotoPath == null) return;
         ImageView photoView = findViewById(R.id.photo_view);
-        photoView.setImageBitmap(photoImg);
+        // Get the dimensions of the View
+        int targetW = photoView.getWidth();
+        int targetH = photoView.getHeight();
+
+        // Get the dimensions of the bitmap
+        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+        bmOptions.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
+        int photoW = bmOptions.outWidth;
+        int photoH = bmOptions.outHeight;
+
+        // Determine how much to scale down the image
+        int scaleFactor = Math.min(photoW / targetW, photoH / targetH);
+
+        // Decode the image file into a Bitmap sized to fill the View
+        bmOptions.inJustDecodeBounds = false;
+        bmOptions.inSampleSize = scaleFactor;
+        bmOptions.inPurgeable = true;
+
+        Bitmap bitmap = BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
+        photoView.setImageBitmap(bitmap);
     }
 
     @Override
@@ -54,8 +89,6 @@ public class MainActivity extends AppCompatActivity {
         switch (reqCode) {
             case REQ_PHOTO:
                 if (resCode == RESULT_OK) {
-                    Bundle extras = data.getExtras();
-                    photoImg = (Bitmap) extras.get("data");
                     showPhoto();
                 }
                 break;
@@ -66,5 +99,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
         showPhoto();
+    }
+
+    private File createImageFile() {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image;
+        try {
+            image = File.createTempFile(
+                    imageFileName,  /* prefix */
+                    ".jpg",         /* suffix */
+                    storageDir      /* directory */
+            );
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
     }
 }
